f5385e84929e461d2577c860075e5ecefea40422
Mateusz Janczak <mateuszjanczak.mj@gmail.com>
2020-11-03 11:25:23 +0100

    Stack operations class has been implemented

--- src/com/mateuszjanczak/Main.java
+++ src/com/mateuszjanczak/Main.java
@@ -3,6 +3,18 @@ package com.mateuszjanczak;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Stack stack = new Stack();
+
+        stack.push("1");
+        stack.push("2");
+        stack.push("3");
+        stack.push("4");
+
+        System.out.println(stack.get());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.get());
     }
 }
