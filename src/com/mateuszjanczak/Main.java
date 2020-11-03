a2447bc4f14ca24446ad0e3546b7d7071eafef46
Mateusz Janczak <mateuszjanczak.mj@gmail.com>
2020-11-03 11:12:56 +0100

    Initial commit, stack interface

---
+++ src/com/mateuszjanczak/Main.java
@@ -0,0 +1,8 @@
+package com.mateuszjanczak;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/mateuszjanczak/StackOperations.java
@@ -0,0 +1,14 @@
+package com.mateuszjanczak;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StackOperations {
+
+    List<String> get();
+
+    Optional<String> pop();
+
+    void push (final String item);
+
+}
