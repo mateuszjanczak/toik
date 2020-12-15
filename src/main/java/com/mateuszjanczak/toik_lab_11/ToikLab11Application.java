d49ba2b605e795603c98ee79f136dc7282d61735
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2020-12-15 10:15:47 +0100

    Initial commit

---
+++ src/main/java/com/mateuszjanczak/toik_lab_11/ToikLab11Application.java
@@ -0,0 +1,13 @@
+package com.mateuszjanczak.toik_lab_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToikLab11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToikLab11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mateuszjanczak/toik_lab_11/ToikLab11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuszjanczak.toik_lab_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToikLab11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
