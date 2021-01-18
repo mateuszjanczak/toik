20c92a0504afed807c0c018ae2fdd5c427f5cd40
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-01-18 21:11:33 +0100

    Refactor code

--- src/main/java/com/mateuszjanczak/toikkolokwium/web/QuadraticController.java
+++ src/main/java/com/mateuszjanczak/toikkolokwium/web/QuadraticController.java
@@ -5,16 +5,13 @@ import com.mateuszjanczak.toikkolokwium.model.Result;
 import com.mateuszjanczak.toikkolokwium.service.QuadraticService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @RestController
 public class QuadraticController {
@@ -28,19 +25,32 @@ public class QuadraticController {
     @GetMapping("/api/math/quadratic-function")
     public ResponseEntity<Object> calculateQuadratic(@RequestParam(required = false) Double a, @RequestParam(required = false) Double b, @RequestParam(required = false) Double c, HttpServletRequest httpServletRequest) {
 
-        ArrayList<String> errors = new ArrayList<>();
+        Optional<Alert> optionalAlert = checkParams(httpServletRequest);
 
-        if(!httpServletRequest.getParameterMap().containsKey("a")) errors.add("Podaj a");
-        if(!httpServletRequest.getParameterMap().containsKey("b")) errors.add("Podaj b");
-        if(!httpServletRequest.getParameterMap().containsKey("c")) errors.add("Podaj c");
-
-        if(errors.size() > 0) {
-            return new ResponseEntity<>(new Alert(errors), HttpStatus.BAD_REQUEST);
+        if(optionalAlert.isPresent()) {
+            return new ResponseEntity<>(optionalAlert.get(), HttpStatus.BAD_REQUEST);
         }
 
         Result result = quadraticService.calculate(a, b, c);
 
         return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    private Optional<Alert> checkParams(HttpServletRequest request) {
+
+        Optional<Alert> optionalAlert = Optional.empty();
+
+        ArrayList<String> errors = new ArrayList<>();
+
+        if(!request.getParameterMap().containsKey("a")) errors.add("Podaj a");
+        if(!request.getParameterMap().containsKey("b")) errors.add("Podaj b");
+        if(!request.getParameterMap().containsKey("c")) errors.add("Podaj c");
+
+        if(errors.size() > 0) {
+            optionalAlert = Optional.of(new Alert(errors));
+        }
+
+        return optionalAlert;
     }
 
     /*@ExceptionHandler(MissingServletRequestParameterException.class)
@@ -49,5 +59,4 @@ public class QuadraticController {
     public Alert handleException(MissingServletRequestParameterException ex) {
         return new Alert("Parametry nie zostały wprowadzone");
     }*/
-
 }
